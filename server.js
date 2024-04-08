e29b5eec73f1f85702de80662bed37846fa3f5c5
Faheem506pk <81044583+Faheem506pk@users.noreply.github.com>
2024-04-09 01:40:23 +0500

    vvv

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const cors_proxy = require('cors-anywhere');
 
 const PORT = process.env.PORT || 8080;
 
-const whitelist = ['https://marketplace-juhuu.vercel.app/'];
+const whitelist = ['marketplace-juhuu.vercel.app/'];
 
 cors_proxy.createServer({
   originWhitelist: whitelist,
