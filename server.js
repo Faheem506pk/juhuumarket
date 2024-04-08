be958c1c11cb1fd79774723eeda0c8611791b407
Faheem506pk <81044583+Faheem506pk@users.noreply.github.com>
2024-04-09 01:36:31 +0500

    bb

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 cors_proxy.createServer({
   originWhitelist: [], // Allow all origins
   requireHeader: ['origin', 'x-requested-with'],
-  removeHeaders: ['cookie', 'cookie2']
+  
 }).listen(PORT, () => {
   console.log(`CORS Anywhere server is running on port ${PORT}`);
 });
