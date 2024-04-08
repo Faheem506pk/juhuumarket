44fb725c3a2120978b9101fd650371353694d5b4
Faheem506pk <81044583+Faheem506pk@users.noreply.github.com>
2024-04-09 01:38:06 +0500

    new

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const cors_proxy = require('cors-anywhere');
 
 const PORT = process.env.PORT || 8080;
 
+const whitelist = ['https://marketplace-juhuu.vercel.app/'];
+
 cors_proxy.createServer({
-  originWhitelist: [], // Allow all origins
+  originWhitelist: whitelist,
   requireHeader: ['origin', 'x-requested-with'],
-  
 }).listen(PORT, () => {
   console.log(`CORS Anywhere server is running on port ${PORT}`);
 });
