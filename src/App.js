3a19d76951e477b15dbf6bc18ff18283de6f1991
Faheem506pk <81044583+Faheem506pk@users.noreply.github.com>
2024-04-12 01:10:17 +0500

    v

---
+++ src/App.js
@@ -0,0 +1,46 @@
+//App.js
+
+import React , { useState }from 'react';
+import {  Routes, Route } from 'react-router-dom';
+import ProductOverviewPage from './components/ProductOverviewPage';
+import Header from './components/header';
+import Footer from './components/Footer';
+import ProductDetailsPage from './components/ProductDetailsPage';
+import { TranslationProvider } from './components/TranslationContext';
+import LanguageSelector from './components/LanguageSelector';
+
+
+
+function App() {
+  const [language, setLanguage] = useState("de");
+
+  const handleLanguageChange = (selectedLanguage) => {
+    setLanguage(selectedLanguage);
+  };
+  return (<div className="App">
+<TranslationProvider>
+  
+    <Header />
+    <div  style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div></div> {/* Left side content */}
+          <LanguageSelector  onChangeLanguage={handleLanguageChange}  /> {/* Right side content */}
+        </div>
+    <Routes>
+    <Route path="/" element={<ProductOverviewPage language={language} />} />
+    
+    <Route path="/v1/products/:productId" element={<ProductDetailsPage language={language}/>} />
+
+
+    </Routes>
+    
+    <Footer/>
+
+    
+    </TranslationProvider>
+    
+   
+</div>
+  );
+}
+
+export default App;
